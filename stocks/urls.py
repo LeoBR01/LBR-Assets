2a40612e9e0dbbc0af7ccb93b308e5093bd6fafa
Leonardo Brasil <leo.br.almeida@gmail.com>
2021-09-02 21:07:40 -0300

    Criando botão stock para testar a chamada da API

--- stocks/urls.py
+++ stocks/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import StockInfo
 
 urlpatterns = [
-    path('stock/', StockInfo.as_view(template_name='stock.html'), name='Stock Info'),
+    path('stock/', StockInfo.as_view(template_name='stock.html'),
+         name='stock'),
 ]
